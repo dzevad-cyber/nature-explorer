5eb7e67406d1b72d0bc6e576524d19ceb0d9df2b
MansoN <dzevad.del@gmail.com>
2020-12-24 21:26:03 +0100

    csp edit 21

--- client/src/pages/home-page/HomePage.js
+++ client/src/pages/home-page/HomePage.js
@@ -1,10 +1,8 @@
-import { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { getTours } from '../../store/tours/toursSlice';
 
 import Map from '../../components/map/Map';
 import styles from './HomePage.module.scss';
-import Loader from '../../components/loader/Loader';
 
 const Home = () => {
   const tours = useSelector(getTours);
